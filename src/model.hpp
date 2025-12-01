23b8853f7b648d47a8e6fa2d73242bcfeedb4b33
PC <pjcoln2004@gmail.com>
2025-12-01 03:27:43 -0700

    Rules added

--- src/model.hpp
+++ src/model.hpp
@@ -1,5 +1,18 @@
 #pragma once
+#include <string>
 #include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+
+struct Rules {
+    // shorts to save some memory since doesnt exceed 32k
+    short max_hours_per_week_default = 40; // default weekly hour limit
+    short max_consecutive_days_default = 5; // default limit for consecutive working days
+    short min_rest_hours_default = 12; // default limit for rest between shifts
+    std::unordered_set<std::string> hard_constraints; // Covereage, legal limits, Needs
+    std::unordered_set<std::string> soft_constraints; // preferences, fairness
+};
 
 struct InputModel {
     std::vector<Staff> staff; // List of staff for schedules
